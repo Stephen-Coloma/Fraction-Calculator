59050f0bec6df9d1fc4ebbd7154d873b285c9b99
Sanchie Guzman <2232886@slu.edu.ph>
2023-03-23 22:41:19 +0800

    SANCHIE - fixed the comments in Arithmetics

--- src/prog2/midgroup01/Arithmetics.java
+++ src/prog2/midgroup01/Arithmetics.java
@@ -5,13 +5,13 @@ import prog2.prelimgroup01.Fraction;
 public interface Arithmetics {
 
     //this part was edited by SANCHIE GUZMAN on 03-17--2023
-    /**These arithmetic methods will be implemented int eh Fraction class*/
+    /**These arithmetic methods will be implemented in the Fraction class*/
     public Fraction add(Fraction other);
-    /**These arithmetic methods will be implemented int eh Fraction class*/
+    /**These arithmetic methods will be implemented in the Fraction class*/
     public Fraction subtract(Fraction other);
-    /**These arithmetic methods will be implemented int eh Fraction class*/
+    /**These arithmetic methods will be implemented in the Fraction class*/
     public Fraction multiplyBy(Fraction other);
-    /**These arithmetic methods will be implemented int eh Fraction class*/
+    /**These arithmetic methods will be implemented in the Fraction class*/
     public Fraction divideBy(Fraction other);
 }
 
